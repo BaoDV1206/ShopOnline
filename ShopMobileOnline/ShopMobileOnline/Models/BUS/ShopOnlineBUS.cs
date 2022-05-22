94a05175b9f7d89d92bd0fa0efde2148cfa44a2a
Jemmyn1206 <baodinhktt3@gmail.com>
2022-05-22 20:01:04 +0700

    upadte lan 1

---
+++ ShopMobileOnline/ShopMobileOnline/Models/BUS/ShopOnlineBUS.cs
@@ -0,0 +1,23 @@
+﻿using ShopMobileOnlineConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopMobileOnline.Models.BUS
+{
+    public class ShopOnlineBUS
+    {
+        public static IEnumerable<SanPham> DanhSach()
+        {
+            var db = new ShopMobileOnlineConnectionDB();
+            return db.Query<SanPham>("select * from SanPham where TinhTrang = 0");
+        }
+        public static SanPham ChiTiet(String masp)
+        {
+            var db = new ShopMobileOnlineConnectionDB();
+            return db.SingleOrDefault<SanPham>("select * from SanPham where MaSanPham = @0",masp);
+        }
+    }
+}
+
